04e843513cbc9094e95c1c0459e636cae5d1d538
Weston Schutt <wtschutt@gmail.com>
2023-09-22 15:42:18 -0600

    Feature: Add Mapquest Service for finding lat/lon from city,state

---
+++ app/services/mapquest_service.rb
@@ -0,0 +1,17 @@
+class MapquestService
+
+  def get_lat_lon(location)
+    get_url("/geocoding/v1/address?location=#{location}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://www.mapquestapi.com") do |faraday|
+      faraday.params["key"] = Rails.application.credentials.mapquest[:key]
+    end
+  end
+end
