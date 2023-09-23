0626564ab114d6c0277d578c134ae4400266c00d
Weston Schutt <wtschutt@gmail.com>
2023-09-23 16:16:00 -0600

    Feature: Update ForecastController #index action

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,9 +1,5 @@
 class Api::V0::ForecastController < ApplicationController
   def index
-    begin
-      render json: ForecastSerializer.new(WeatherFacade.new(params[:location]).forecast)
-    rescue StandardError => e
-      require 'pry'; binding.pry
-    end
+    render json: ForecastSerializer.new(WeatherFacade.new(params[:location]).forecast)
   end
 end
