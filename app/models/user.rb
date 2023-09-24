614c87a8cdd16d49e53b3e63a7ab68d829427fb3
Weston Schutt <wtschutt@gmail.com>
2023-09-24 13:53:39 -0600

    Feature: Update User to use a callback #generate_api_key function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,10 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true
   validates :api_key, presence: true, length: { is: 36 }
   has_secure_password
+  
+  before_validation :generate_api_key, on: :create
 
-  def self.create_with_api_key(user_params) 
-    user = User.create do |user|
-      user.email = user_params[:email]
-      user.password = user_params[:password]
-      user.password_confirmation = user_params[:password_confirmation]
-      user.api_key = SecureRandom.uuid
-    end
-    user
+  def generate_api_key
+    self.api_key = SecureRandom.uuid
   end
 end
