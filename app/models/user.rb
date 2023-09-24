d37adbdea2b70a4eac436d1425f1f2a1598609b3
Weston Schutt <wtschutt@gmail.com>
2023-09-24 13:02:42 -0600

    Feature: Add User #create_with_api_key model method and api_key attribute validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, confirmation: true
+  validates :api_key, presence: true, length: { is: 36 }
   has_secure_password
 
-  
+  def self.create_with_api_key(user_params) 
+    user = User.create do |user|
+      user.email = user_params[:email]
+      user.password = user_params[:password]
+      user.password_confirmation = user_params[:password_confirmation]
+      user.api_key = SecureRandom.uuid
+    end
+    user
+  end
 end
