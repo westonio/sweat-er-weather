36a0a369a0ddd17ed895588880ef59009ff3dd5c
Weston Schutt <wtschutt@gmail.com>
2023-09-25 11:27:31 -0600

    Feature: Add Book Poro with instance methods to format books and weather data

--- app/poros/book.rb
+++ app/poros/book.rb
@@ -1,10 +1,36 @@
 class Book
-  attr_reader :destination,
+  attr_reader :id,
+              :destination,
               :forecast,
               :total_books_found,
               :books
 
-  def initializer(data)
-    
+  def initialize(destination, weather, book_data)
+    @id = nil
+    @destination = destination
+    @forecast = format_weather(weather.current)
+    @total_books_found = book_data[:numFound]
+    @books = book_results(book_data[:docs])
+  end
+
+private
+  def book_results(book_data)
+    book_data.map do |book|
+      format_book(book)
+    end
+  end
+  
+  def format_book(book)
+    {
+      isbn: book[:isbn],
+      title: book[:title]
+    }
+  end
+
+  def format_weather(weather)
+    {
+      summary: weather[:condition],
+      temperature: weather[:temperature]
+    }
   end
 end
